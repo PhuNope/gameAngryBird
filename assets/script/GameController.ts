4d3ab9115520d0e54f50bc975c0df05ca3c334da
Nope <mrphu2001@gmail.com>
2022-12-28 00:13:51 +0700

    pre work

--- assets/script/GameController.ts
+++ assets/script/GameController.ts
@@ -1,14 +1,23 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Prefab } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameController')
 export class GameController extends Component {
+    private gameLevel: number = 1;
+    private currentLevelNode: Node | null = null;
+    private bird: Node | null = null;
+    private birdPrefab: Prefab | null = null;
+
+    @property(Node)
+    private ui_draw: Node | null = null;
+
     start() {
 
     }
 
     update(deltaTime: number) {
-        
+
     }
 }
-
+
+
